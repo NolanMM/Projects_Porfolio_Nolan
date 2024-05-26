f012d8681bdbc2bfa46ee3383c38f90446d29373
NolanM <minhlenguyen7@gmail.com>
2024-05-26 14:53:08 -0400

    first commit

---
+++ 1_HomePage.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title='Home Page', page_icon=':house:', layout='wide', initial_sidebar_state='auto')
+
+st.title('Home Page')
+
+st.write('Welcome to the Home Page')
---
+++ pages/2_Data_Science_Projects.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title='Data Science Project', page_icon=':house:', layout='wide', initial_sidebar_state='auto')
+
+st.title('Data Science Project')
+
+st.write('Welcome to the Data Science Project')
